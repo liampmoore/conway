d18d97e21933317355ebda825cb8c667d8a7463e
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-07-28 19:40:45 -0400

    adds checkcell helper function pseudocode

--- src/generateCells.js
+++ src/generateCells.js
@@ -1,4 +1,12 @@
-function generateCells(currentMap) {
+function checkCell(currentMap, cell, nextMap, deadMap) {
+  // Check if the current cell is in the nextMap already, if so return
+  // Check if the current cell is in the deadMap already, if so return
+  // Otherwise use the cells coordinates to count how many neighbors it has in the currentmap
+  // If it doesn't have two or three living neighbors in the current frame set it to the deadMap for the next frame
+  // If it has three living neighbors in the current frame set it to nextMap, alive for the next frame
+}
+
+function generateCells(currentMap, xSize, ySize) {
   if (!input[0].length) {
     console.log(typeof input[0]);
     throw new Error("inputGrid is not properly formatted.");
@@ -12,18 +20,39 @@ function generateCells(currentMap) {
   deadMap = new Map();
 
   // Iterate through the old map, this will loop through every alive cell in the current frame.
-  for (let [key, value] of currentMap) {
+  for (let [key, cell] of currentMap) {
     // Check neighbors
     // For each one check if it's already in the nextMap, and if it's in the deadMap
     // If it isn't in either of those run the checkCell helper function on it
-    // Top left
-    // Top
-    // Top right
-    // Left
-    // Right
-    // Bottom left
-    // Bottom
-    // Bottom right
+
+    // Check the top row of neighbors if the current cell isn't on the top row of the screen
+    if (cell.y > 0) {
+      if (cell.x > 0) {
+        // Top left
+      }
+      // Top
+      if (cell.x < xSize - 1) {
+        // Top right
+      }
+    }
+    // Check the middle left neighbor if the cell isn't on the leftmost side of the screen
+    if (cell.x > 0) {
+      // Left
+    }
+    // Check the middle right neighbor if the cell isn't on the rightmost side of the screen
+    if (cell.x < xSize - 1) {
+      // Right
+    }
+    // Check the bottom row of neighbors if the current cell isn't on the bottom side of the screen
+    if (cell.y < ySize - 1) {
+      if (cell.x > 0) {
+        // Bottom left
+      }
+      // Bottom
+      if (cell.x < xSize - 1) {
+        // Bottom right
+      }
+    }
   }
 
   return nextMap;
