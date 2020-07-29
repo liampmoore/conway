487fcaf1175a2988b9b3af17389b59b56e04c1a5
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-07-29 16:21:35 -0400

    adds styled-components and normalize.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  height: 100vh;
+  background-color: red;
+`;
 
 function App() {
-  return <></>;
+  return <Wrapper></Wrapper>;
 }
 
 export default App;
