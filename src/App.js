71986e00fc9844975449f517b648efa5d7c4ae27
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-07-31 12:43:45 -0400

    adds github link

--- src/App.js
+++ src/App.js
@@ -68,6 +68,7 @@ function App() {
         />
         <Viewport
           isPlaying={isPlaying}
+          setIsPlaying={setIsPlaying}
           cellMap={cellMap}
           setCellMap={setCellMap}
         />
--- src/components/Viewport.js
+++ src/components/Viewport.js
@@ -11,25 +11,30 @@ const ViewportContainer = styled.div`
   border-bottom-right-radius: 5px;
   width: 100%;
   border: 0.5px solid #bcc4e3;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
 `;
 
 const TitleScreen = styled.div`
   position: absolute;
   z-index: 100;
   width: 100%;
-  top: 25%;
+  top: 20%;
 `;
 
 const Title = styled.h1`
   font-size: 1.2rem;
   text-align: center;
   font-family: "Mulish", sans-serif;
-  width: 100%;
 `;
 
 const Link = styled.a`
   color: black;
   text-decoration: none;
+  display: block;
   :active {
     color: #ffaaff;
     text-decoration: underline;
@@ -40,12 +45,23 @@ const Link = styled.a`
   }
 `;
 
+const ImgLink = styled.a`
+  width: 32px;
+  color: black;
+  display: block;
+  align-self: center;
+  margin: 0 auto;
+  img {
+    width: 32px;
+    text-align: center;
+  }
+`;
+
 const Description = styled.p`
   font-size: 1rem;
   width: 100%;
   text-align: center;
   font-family: "Mulish", sans-serif;
-  pointer-events: none;
   -webkit-touch-callout: none; /* iOS Safari */
   -webkit-user-select: none; /* Safari */
   -khtml-user-select: none; /* Konqueror HTML */
@@ -55,23 +71,66 @@ const Description = styled.p`
                                   supported by Chrome, Edge, Opera and Firefox */
 `;
 
-export default function Viewport({ isPlaying, cellMap, setCellMap }) {
+export default function Viewport({
+  isPlaying,
+  setIsPlaying,
+  cellMap,
+  setCellMap,
+}) {
+  const handleClick = () => {
+    setCellMap({
+      "1409": {
+        y: 14,
+        x: 9,
+      },
+      "1310": {
+        y: 13,
+        x: 10,
+      },
+      "1210": {
+        y: 12,
+        x: 10,
+      },
+      "1209": {
+        y: 12,
+        x: 9,
+      },
+      "1208": {
+        y: 12,
+        x: 8,
+      },
+    });
+  };
   return (
     <ViewportContainer>
       {Object.keys(cellMap).length === 0 ? (
         <TitleScreen>
           <Title>
-            <Link href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+            >
               Conway's Game of Life
             </Link>
           </Title>
-          <Description>
+          <Description onClick={() => handleClick()}>
             Click empty space to add cells. <br />
             <br />
             Click on a cell to remove it. <br />
             <br />
-            Press play to start the game.
+            Press <span style={{ color: "#70FFAF" }}>play</span> to start the
+            game.
+            <br />
+            <br />
           </Description>
+          <ImgLink
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/liampmoore/conway"
+          >
+            <img src="github.png"></img>
+          </ImgLink>
         </TitleScreen>
       ) : (
         <></>
