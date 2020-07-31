018ec2abd8c3d36b63785a8dfbaf4d3edba9a334
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-07-31 13:13:24 -0400

    adjusts title and links

--- src/components/Viewport.js
+++ src/components/Viewport.js
@@ -22,19 +22,34 @@ const TitleScreen = styled.div`
   position: absolute;
   z-index: 100;
   width: 100%;
-  top: 24%;
+  top: 20%;
 `;
 
 const Title = styled.h1`
   font-size: 1.1rem;
   text-align: center;
   font-family: "Mulish", sans-serif;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 `;
 
 const Link = styled.a`
   color: black;
   text-decoration: none;
-  display: inline;
+  font-family: "Mulish", sans-serif;
+  font-size: 0.94rem;
+  text-align: center;
+  font-weight: bold;
+  align-self: center;
+  width: 140px;
+  display: block;
+  margin: 0 auto;
+
   :active {
     color: #ffaaff;
     text-decoration: underline;
@@ -46,13 +61,13 @@ const Link = styled.a`
 `;
 
 const ImgLink = styled.a`
-  width: 32px;
+  width: 1.6rem;
   color: black;
   display: block;
   align-self: center;
   margin: 0 auto;
   img {
-    width: 32px;
+    width: 1.6rem;
     text-align: center;
   }
 `;
@@ -69,6 +84,7 @@ const Description = styled.p`
   -ms-user-select: none; /* Internet Explorer/Edge */
   user-select: none; /* Non-prefixed version, currently
                                   supported by Chrome, Edge, Opera and Firefox */
+  margin-bottom: 1.12rem;
 `;
 
 export default function Viewport({
@@ -105,25 +121,26 @@ export default function Viewport({
     <ViewportContainer>
       {Object.keys(cellMap).length === 0 ? (
         <TitleScreen>
-          <Title>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
-            >
-              Conway's Game of Life
-            </Link>
-          </Title>
+          <Title>Conway's Game of Life</Title>
           <Description onClick={() => handleClick()}>
-            Click empty space to add cells. <br />
+            Click empty space to add living cells. <br />
             <br />
             Click on a cell to remove it. <br />
             <br />
             Press <span style={{ color: "#70FFAF" }}>play</span> to start the
             game.
             <br />
-            <br />
           </Description>
+
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+          >
+            Further reading
+          </Link>
+          <br />
+
           <ImgLink
             target="_blank"
             rel="noopener noreferrer"
